635efb08b25e17b5a08469da971fb4facc7a7d6f
quoctrung163 <quoctrung163@yandex.com>
2020-02-27 17:08:34 +0700

    Updates

---
+++ precache-manifest.4ec749ac6653611bd9506d0d9655713b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0993d81fe2d5d1f1a8ce26260f5bc5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9c94a7da06d85f4e01d2",
+    "url": "/monster-rolodex/static/css/main.42915eb6.chunk.css"
+  },
+  {
+    "revision": "803ee1468e32a5bd4b4c",
+    "url": "/monster-rolodex/static/js/2.3b2b3505.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.3b2b3505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c94a7da06d85f4e01d2",
+    "url": "/monster-rolodex/static/js/main.5bb4a9b8.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
